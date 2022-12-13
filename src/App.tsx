22f3476c7e4178985189a209e21721d3c9068982
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-13 16:09:19 +0700

    App: Implement ApiRequestUi component
    Import and implement the component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import ApiRequestUi from "./components/ApiRequestUi";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ApiRequestUi />
+    </div>
+  );
 }
 
 export default App;
