cc15f94f7ebb5ec7ea148a87cfc64739d529a0ba
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-13 20:05:35 +0700

    App: Implement ProxySelector
    Import and implement the ProxySelector. Then create state to track proxy url and pass it and the setter to the new component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import UrlSubmitter from "./components/UrlSubmitter";
+import ProxySelector from "./components/ProxySelector";
 import "./App.css";
 
 function App() {
+  const [proxyUrl, setProxyUrl] = useState("");
+
   return (
     <div className="App">
       <UrlSubmitter />
+      <ProxySelector proxyUrl={proxyUrl} setProxyUrl={setProxyUrl} />
     </div>
   );
 }
