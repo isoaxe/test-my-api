f857e2b105458d9b774f6de40d75b91f2a6f54a6
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-15 13:30:10 +0700

    App: Add the new component
    Import and implement the new HeadingText component at the top of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import UrlSubmitter from "./components/UrlSubmitter";
 import ProxySelector from "./components/ProxySelector";
+import HeadingText from "./components/HeadingText";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <HeadingText />
       <UrlSubmitter proxyUrl={proxyUrl} />
       <ProxySelector proxyUrl={proxyUrl} setProxyUrl={setProxyUrl} />
     </div>
