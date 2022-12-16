294812097e2519779bbe5e7e6179177b07968099
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-16 17:03:27 +0700

    App: Refactor styles
    Delete the separate CSS file and instead use the Box component from MUI to implement inline styles as with other components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
+import { Box } from "@mui/material";
 import UrlSubmitter from "./components/UrlSubmitter";
 import ProxySelector from "./components/ProxySelector";
 import HeadingText from "./components/HeadingText";
 import AboutText from "./components/AboutText";
 import SampleUrls from "./components/SampleUrls";
-import "./App.css";
 
 function App() {
   const [proxyUrl, setProxyUrl] = useState("");
 
   return (
-    <div className="App">
+    <Box sx={appStyle}>
       <HeadingText />
       <AboutText />
       <SampleUrls />
@@ -18,8 +18,18 @@ function App() {
       <UrlSubmitter proxyUrl={proxyUrl} />
       <UrlSubmitter proxyUrl={proxyUrl} />
       <ProxySelector proxyUrl={proxyUrl} setProxyUrl={setProxyUrl} />
-    </div>
+    </Box>
   );
 }
 
 export default App;
+
+const appStyle = {
+  margin: "auto",
+  width: "95%",
+  maxWidth: " 800px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
