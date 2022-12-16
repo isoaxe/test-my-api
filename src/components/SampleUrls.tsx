d121d2bf719f7572d8eb9b022ddf3b440d218ffd
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-16 16:15:32 +0700

    Create SampleUrls.tsx
    A heading and two sample URLs with a copy function so they can be quickly pasted into the API endpoint field.

---
+++ src/components/SampleUrls.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Box, Typography, Snackbar, Alert } from "@mui/material";
+import { POSTMAN_GET, POSTMAN_POST } from "../util/urls";
+
+function SampleUrls() {
+  const [showCopyAlert, setShowCopyAlert] = useState(false);
+
+  // Helper function to return a URL and copy icon.
+  function SingleUrl(props: any) {
+    const { url } = props;
+
+    function handleClick() {
+      setShowCopyAlert(true);
+      navigator.clipboard.writeText(url);
+    }
+
+    return (
+      <Box sx={urlWrapper}>
+        <Typography variant="body1" component="span" sx={urlStyle}>
+          {url}
+        </Typography>
+        <ContentCopyIcon
+          onClick={handleClick}
+          color="primary"
+          sx={copyIconStyle}
+        />
+      </Box>
+    );
+  }
+
+  return (
+    <Box sx={sampleWrapper}>
+      <Typography variant="h6" component="h6" sx={headingStyle}>
+        Sample API endpoints for testing
+      </Typography>
+      <SingleUrl url={POSTMAN_GET} />
+      <SingleUrl url={POSTMAN_POST} />
+      <Snackbar
+        open={showCopyAlert}
+        onClose={() => setShowCopyAlert(false)}
+        autoHideDuration={3000}>
+        <Alert severity="info" variant="outlined">
+          Copied to clipboard
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
+}
+
+export default SampleUrls;
+
+const sampleWrapper = {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+};
+
+const headingStyle = {
+  width: "100%",
+};
+
+/* Below styles are for <SingleUrl/> helper component. */
+
+const urlStyle = {
+  fontSize: "18px",
+  opacity: "70%",
+};
+
+const urlWrapper = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  maxWidth: "280px",
+  marginBottom: "5px",
+};
+
+const copyIconStyle = {
+  cursor: "pointer",
+};
