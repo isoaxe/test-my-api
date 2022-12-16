b6749102c35ef09aa1fa979357a69f704213206a
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-16 14:57:50 +0700

    urls: Add endpoint urls
    These will be displayed to the user as samples to go test out the utility.

--- src/util/urls.ts
+++ src/util/urls.ts
@@ -3,3 +3,7 @@
 // CORS-Anywhere reverse proxy URLs that users can prepend to requests.
 export const FIREBASE_PROXY = "https://proxy-ibmasyzzya-uc.a.run.app/";
 export const FLY_PROXY = "https://cors-server.fly.dev/";
+
+// Sample endpoint URLs for the user to test the utility with
+export const POSTMAN_GET = "postman-echo.com/get";
+export const POSTMAN_POST = "postman-echo.com/post";
