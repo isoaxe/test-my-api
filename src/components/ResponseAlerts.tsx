9cf65e427bf8a0880fe4707a2cda63f94ed2944b
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-17 18:27:36 +0700

    ResponseAlerts: Change alert severity based off status code
    Calculate response category based on response code. Then set the alertStatus to the appropriate AlertColor enum based off this.

--- src/components/ResponseAlerts.tsx
+++ src/components/ResponseAlerts.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
-import { Snackbar, Alert, AlertTitle } from "@mui/material";
+import { useState, useEffect, useRef } from "react";
+import { Snackbar, Alert, AlertTitle, AlertColor } from "@mui/material";
 
 function ResponseAlerts(props: any) {
   const [showResponse, setShowResponse] = useState(false);
+  const alertStatus = useRef<AlertColor>("info");
 
   const { response, responseCode } = props;
   const vertical = "top";
@@ -14,12 +15,25 @@ function ResponseAlerts(props: any) {
     }
   }, [response]);
 
+  useEffect(() => {
+    const codeCategory = Math.floor(responseCode / 100);
+    if (codeCategory === 2) {
+      alertStatus.current = "success"; // HTTP code 200 range
+    } else if (codeCategory === 4) {
+      alertStatus.current = "warning"; // HTTP code 400 range
+    } else if (codeCategory === 5) {
+      alertStatus.current = "error"; // HTTP code 500 range
+    } else {
+      alertStatus.current = "info";
+    }
+  }, [responseCode]);
+
   return (
     <Snackbar
       open={showResponse}
       onClose={() => setShowResponse(false)}
       anchorOrigin={{ vertical, horizontal }}>
-      <Alert severity="info" variant="filled" sx={alertStyle}>
+      <Alert severity={alertStatus.current} variant="filled" sx={alertStyle}>
         {responseCode && <AlertTitle>Status: {responseCode}</AlertTitle>}
         {JSON.stringify(response, null, 4)}
       </Alert>
