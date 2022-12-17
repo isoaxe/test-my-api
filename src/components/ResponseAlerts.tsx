d1dac944ab2b6ce5e5b4a49b2aa24ee122d2b7df
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-17 15:32:34 +0700

    ResponseAlerts: Show alert when response received
    + Take response from props and implement a useEffect hook to set the alert to visible when a response has been received.
    + Reposition alert to the top centre.
    + Change the alert style to filled so that it is easier to read when overlaid on the rest of the UI.
    + Display the response in the alert.

--- src/components/ResponseAlerts.tsx
+++ src/components/ResponseAlerts.tsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Snackbar, Alert } from "@mui/material";
 
-function ResponseAlerts() {
+function ResponseAlerts(props: any) {
   const [showResponse, setShowResponse] = useState(false);
 
+  const { response } = props;
+  const vertical = "top";
+  const horizontal = "center";
+
+  useEffect(() => {
+    if (Object.keys(response).length) {
+      setShowResponse(true);
+    }
+  }, [response]);
+
   return (
-    <Snackbar open={showResponse} onClose={() => setShowResponse(false)}>
-      <Alert severity="info" variant="outlined">
-        Placeholder Text
+    <Snackbar
+      open={showResponse}
+      onClose={() => setShowResponse(false)}
+      anchorOrigin={{ vertical, horizontal }}>
+      <Alert severity="info" variant="filled">
+        {JSON.stringify(response)}
       </Alert>
     </Snackbar>
   );
