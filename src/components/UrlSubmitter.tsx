936ad4d0f2fb71eef6bdebad1b0032369ece3976
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-12-17 21:22:10 +0700

    UrlSubmitter: Remove reset of response
    No need to reset the response object here when the code is not 200. It is being reset when the alert closes instead.

--- src/components/UrlSubmitter.tsx
+++ src/components/UrlSubmitter.tsx
@@ -30,8 +30,6 @@ function UrlSubmitter(props: any) {
         const jsonRes = await res.json();
         setResponse(jsonRes);
         console.log(jsonRes);
-      } else {
-        setResponse({});
       }
     } catch (error) {
       console.log(error);
