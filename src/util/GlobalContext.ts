06e36ca0c1b8f145653c871a592c556baf2d36e5
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-01-11 22:56:12 +0700

    GlobalContext: Add setters and type interface
    The reason setter methods were previously showing an error was because the type was inferred incorrectly from the placeholder value.
    Use an interface to explicitly state types and thus resolve the critical errors.

--- src/util/GlobalContext.ts
+++ src/util/GlobalContext.ts
@@ -1,7 +1,20 @@
 import { createContext } from "react";
+import { Dispatch, SetStateAction } from "react";
 
-export const GlobalContext = createContext({
+export const GlobalContext = createContext<ContextProps>({
   proxyUrl: "",
   requestType: "GET",
   requestBody: "",
+  setResponse: () => <object>{},
+  setResponseCode: () => <number>{},
+  setResponseCodeText: () => <string>{},
 });
+
+interface ContextProps {
+  proxyUrl: string;
+  requestType: string;
+  requestBody: string;
+  setResponse: Dispatch<SetStateAction<object>>;
+  setResponseCode: Dispatch<SetStateAction<number>>;
+  setResponseCodeText: Dispatch<SetStateAction<string>>;
+}
