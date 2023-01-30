b4e6886908bafd741e727ee71871e4e3d2929ca2
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-01-30 20:02:39 +0700

    GlobalContext: Add spinner state
    Add spinner visibility state to context object and type signature.

--- src/util/GlobalContext.ts
+++ src/util/GlobalContext.ts
@@ -5,6 +5,7 @@ export const UrlSubContext = createContext<UrlSubState>({
   proxyUrl: "",
   requestType: "GET",
   requestBody: "",
+  spinnerOn: false,
   setResponse: () => <object>{}, // eslint-disable-line
   setResponseCode: () => <number>{}, // eslint-disable-line
   setResponseCodeText: () => <string>{}, // eslint-disable-line
@@ -15,6 +16,7 @@ interface UrlSubState {
   proxyUrl: string;
   requestType: string;
   requestBody: string;
+  spinnerOn: boolean;
   setResponse: Dispatch<SetStateAction<object>>;
   setResponseCode: Dispatch<SetStateAction<number>>;
   setResponseCodeText: Dispatch<SetStateAction<string>>;
